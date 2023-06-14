886eab01aabdf8cc6dd4cfc64383f45b88d43fb1
WBwithSuraj <surajkrsr98@gmail.com>
2023-06-14 20:53:01 +0530

    day2

--- electronics-ecom/src/pages/AllPages.tsx
+++ electronics-ecom/src/pages/AllPages.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, Routes} from 'react-router-dom'
 import { Home } from './Home'
 import { Product } from './Product'
+import { Admin } from './Admin'
 export const AllPages = () => {
     return(
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/products' element={<Product/>}/>
+        <Route path='/admin' element={<Admin/>}/>
       </Routes>
     )
 }
