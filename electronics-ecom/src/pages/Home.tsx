5fa2bf71eb87446a7a14699f07c814ad516d3e92
Suraj Kamat <111695568+WDwithSuraj@users.noreply.github.com>
2023-06-17 14:16:41 +0530

    day5

--- electronics-ecom/src/pages/Home.tsx
+++ electronics-ecom/src/pages/Home.tsx
@@ -24,6 +24,23 @@ export const Home = () => {
           <h2>GST Billing</h2>
           </div>
         </DIV>
+        <HOMEImage className='homeImage'>
+          <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/vinambia/10thanniversary/D81867058_PC_Header_1500x450_1.gif" alt="" />
+        </HOMEImage>
+        <ACTOR>
+          <div><img src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/Fitness_Freak_2.png?v=1685083745" alt="" />
+          <h1>For Fitness</h1>
+          </div>
+          <div><img src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/Party_Animal_4.png?v=1685942349" alt="" />
+          <h1>For Parties</h1>
+          </div>
+          <div><img src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/Workaholic_2.png?v=1685083745" alt="" />
+          <h1>For Work</h1>
+          </div>
+          <div><img src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/Entertainment_2.png?v=1685083746" alt="" />
+          <h1>For Audiophiles</h1>
+          </div>
+        </ACTOR>
       </div>
     </>
   )
@@ -43,8 +60,31 @@ const DIV = styled.div`
     margin: 0;
     padding: 0;
   };
+  div h {
+    margin: 0;
+    padding: 0;
+  }
+  
+`
+const ACTOR = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  width:90%;
+  margin: auto;
+  margin-top: 50px;
+  margin-bottom : 50px;
+  div img{
+   width: 310px;
+   height: 500px;
+    margin: 0;
+    padding: 0;
+  };
   div h2 {
     margin: 0;
     padding: 0;
   }
+  
+`
+const HOMEImage = styled.div`
+ margin-left: 50px;
 `
